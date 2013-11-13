0cb069c54117f0a142b2daa5d605b6216362ae49
Prashant Sahni <prashant.sahni5@gmail.com>
2013-11-13 19:02:44 +0530

    module instead of class, because instantiation of HeatIndex class is not needed

--- lib/heat_index.rb
+++ lib/heat_index.rb
@@ -1,5 +1,5 @@
 #Formula reference http://www.srh.noaa.gov/images/ffc/pdf/ta_htindx.PDF
-class HeatIndex
+module HeatIndex
 
   # 
   # This method uses an approximation. 
@@ -9,8 +9,11 @@ class HeatIndex
   #
   # * +t+ - temperature in F
   # * +r+ - relative humidity
+  #HI = c_1 + c_2 T + c_3 R + c_4 T R + c_5 T^2 + c_6 R^2 + c_7 T^2R + c_8 T R^2 + c_9 T^2 R^2
+ 
   def self.calculate(t, r)
     heat_index = -42.379 + 2.04901523*t + 10.14333127*r - 0.22475541*t*r - 6.83783*10**-3*t**2 - 5.481717*10**-2*r**2 +
         1.22874*10**-3*t**2*r + 8.5282*10**-4*t*r**2 - 1.99*10**-6*t**2*r**2
   end
+  
 end
