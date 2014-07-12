e6064a8ac8078701da83435a1130b608fb3b6997
sagar arlekar <sagar.arlekar@gmail.com>
2014-07-12 16:44:35 +0530

    changed documentation

--- lib/heat_index.rb
+++ lib/heat_index.rb
@@ -1,4 +1,4 @@
-#Formula reference http://www.srh.noaa.gov/images/ffc/pdf/ta_htindx.PDF
+# Formula reference http://www.srh.noaa.gov/images/ffc/pdf/ta_htindx.PDF
 module HeatIndex
 
   # 
@@ -9,7 +9,9 @@ module HeatIndex
   #
   # * +t+ - temperature in F
   # * +r+ - relative humidity
-  #HI = c_1 + c_2 T + c_3 R + c_4 T R + c_5 T^2 + c_6 R^2 + c_7 T^2R + c_8 T R^2 + c_9 T^2 R^2
+  # HI = c_1 + c_2 T + c_3 R + c_4 T R + c_5 T^2 + c_6 R^2 + c_7 T^2R + c_8 T R^2 + c_9 T^2 R^2
+  # HI is heat index
+  # c_1, c_2, c_3... are constants  
  
   def self.calculate(t, r)
     heat_index = -42.379 + 2.04901523*t + 10.14333127*r - 0.22475541*t*r - 6.83783*10**-3*t**2 - 5.481717*10**-2*r**2 +
